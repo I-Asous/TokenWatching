cdb9f0a09696160007fb1a630cfd51175a2cfd4e
Alej723 <149021081+Alej723@users.noreply.github.com>
2026-09-23 18:28:25 -0400

    Update extension frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 function App() {
   return (
-    <div>
+    <>
+    <div className="Logo">
       <h1>Token Watching</h1>
-      <p>Extension is working!</p>
+      </div>
+
+      <div className="Board1">
+      <h1>Login</h1>
+
+      <div className="input-login">
+      <input type="text" name="username-id" placeholder="Username" />
+      <input type="text" name="password-id" placeholder="Password" />
+      </div>
+
+      <div className="buttons">
+        <button>Login</button>
+        <button>Signup</button>
+      </div>
+        <a href="">forgot passoword</a>
     </div>
+    </>
   );
 }
 
