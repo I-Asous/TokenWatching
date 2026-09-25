d9432d7c94380b3922557bf172a3fa049d05d6c8
maida5 <145588313+maida5@users.noreply.github.com>
2026-09-24 20:50:33 -0400

    implemented post, delete endpoints

--- backend/service/db-queries.py
+++ backend/service/db-queries.py
@@ -13,6 +13,8 @@ key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
 ### GET METHODS
+# we can change this into upsert
+
 ## this may seem redundant but i decided to implement all of them, but imo i think if we want to see a prompt, we will def want to see the optimized version alongside it as well :/
 # return a specific users original prompts
 def get_user_input_prompts(user_id):
@@ -46,7 +48,7 @@ def get_user_optimized_prompts(user_id):
 def get_user_input_and_optimized_prompts(user_id):
    try:
       # query the database
-      response = supabase.table("prompt").select("*").eq("user_id", user_id).execute()
+      response = supabase.table("prompt").select("*, optimized_prompts(*)").eq("user_id", user_id).execute()
 
       # if there is data, return it
       # need to figure what to do if there is no data... hmmhmmhmm
@@ -60,7 +62,7 @@ def get_user_input_and_optimized_prompts(user_id):
 def get_user_prompt_w_optimized(user_id, prompt_id):
    try:
       # query the database
-      response = supabase.table("prompt").select("*, optimized_prompts(*)").eq("user_id", user_id).execute()
+      response = supabase.table("prompt").select("*, optimized_prompts(*)").eq("user_id", user_id).eq("prompt_id", prompt_id).execute()
 
       # if there is data, return it
       # need to figure what to do if there is no data... hmmhmmhmm
@@ -74,21 +76,97 @@ def get_user_prompt_w_optimized(user_id, prompt_id):
 ### POST METHODS
 
 # send a prompt to the database :3
+# prompt needs to be properly defined
+def create_prompt(prompt):
+   try:
+      # insert into database
+      response = (
+         supabase.table("prompt")
+         .insert(prompt)
+         .execute()
+      )
+      # if there is data, return it
+      # need to figure what to do if there is no data... hmmhmmhmm
+      return response.data
+      
+   # double check this later to see if this exception is the correct one!
+   except Exception as e:
+      raise RuntimeError(f"Database error: {str(e)}")
 
-# send an optimized prompt to the database :3 (but i also think that this should just be a regular function, but who knows what can happen?)
-## trying to think of a way this can be used... :/
+
+# send an optimized prompt to the database :3
+def create_optimized_prompt(prompt):
+   try:
+      # insert into database
+      response = (
+         supabase.table("optimized_prompts")
+         .insert(prompt)
+         .execute()
+      )
+      # if there is data, return it
+      # need to figure what to do if there is no data... hmmhmmhmm
+      return response.data
+      
+   # double check this later to see if this exception is the correct one!
+   except Exception as e:
+      raise RuntimeError(f"Database error: {str(e)}")
 
 ### DELETE METHODS
 # delete user 
 ## since enzo is security guy, lets discuss how we should do this bc in a real app they have like secuirty measures to prevent accidental deletion
-## but this might be a later issue!
+def delete_user(user_id):
+   try:
+      # insert into database
+      response = (
+         supabase.table("users")
+         .delete()
+         .eq("id", user_id)
+         .execute()
+      )
+      # if there is data, return it
+      # need to figure what to do if there is no data... hmmhmmhmm
+      return response.data
+      
+   # double check this later to see if this exception is the correct one!
+   except Exception as e:
+      raise RuntimeError(f"Database error: {str(e)}")
 
 # delete prompt (which also should delete the optimized prompt who is with me!)
-
-# delete optimize (don't even think we need this but it's alwaus good... to have soemthing!)
+def delete_prompt(prompt_id):
+   try:
+      # insert into database
+      response = (
+         supabase.table("users")
+         .delete()
+         .eq("id", prompt_id)
+         .execute()
+      )
+      # if there is data, return it
+      # need to figure what to do if there is no data... hmmhmmhmm
+      return response.data
+      
+   # double check this later to see if this exception is the correct one!
+   except Exception as e:
+      raise RuntimeError(f"Database error: {str(e)}")
 
 ### UPDATE METHOD
-# update user: self explanatory...
+# update user: self explanatory... but hoenslty might change this into an upsert
+def update_user(user, user_id):
+   try:
+      # insert into database
+      response = (
+         supabase.table("users")
+         .update(user)
+         .eq("id", user_id)
+         .execute()
+      )
+      # if there is data, return it
+      # need to figure what to do if there is no data... hmmhmmhmm
+      return response.data
+      
+   # double check this later to see if this exception is the correct one!
+   except Exception as e:
+      raise RuntimeError(f"Database error: {str(e)}")
 
 # update prompt/optimized prompt: not sure as a now? i thikn if the user modifies the original prompt. but i think that should just not be able to go thru...
-
+# this can just be an upsert
