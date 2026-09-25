5a1f38e7252cafc8ca50fcf8d135a1d21b1ee5f7
maida5 <145588313+maida5@users.noreply.github.com>
2026-09-24 20:24:44 -0400

    started GET function implementations and more organized folders

---
+++ backend/service/db-queries.py
@@ -0,0 +1,94 @@
+# i don't like this file name. lmk what we should change it to
+
+# this is just a separate file to create functions that query/insert/delete from the database
+# could i just do this in the api itself? why yes, but if we want this to scale and perhaps change it 
+# to another database, then it be annoying to change every single place that calls supabase api
+# so we have this solution to be able to change stuff. woohoo!
+
+import os
+from supabase import create_client, Client
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+### GET METHODS
+## this may seem redundant but i decided to implement all of them, but imo i think if we want to see a prompt, we will def want to see the optimized version alongside it as well :/
+# return a specific users original prompts
+def get_user_input_prompts(user_id):
+   try:
+      # query the database
+      response = supabase.table("prompt").select("*").eq("user_id", user_id).execute()
+
+      # if there is data, return it
+      # need to figure what to do if there is no data... hmmhmmhmm
+      return response.data[0]
+      
+   # double check this later to see if this exception is the correct one!
+   except Exception as e:
+      raise RuntimeError(f"Database error: {str(e)}")
+
+# return a specific users optimized prompts
+def get_user_optimized_prompts(user_id):
+   try:
+      # query the database
+      response = supabase.table("optimized_prompts").select("*").eq("user_id", user_id).execute()
+
+      # if there is data, return it
+      # need to figure what to do if there is no data... hmmhmmhmm
+      return response.data[0]
+      
+   # double check this later to see if this exception is the correct one!
+   except Exception as e:
+      raise RuntimeError(f"Database error: {str(e)}")
+
+# return specific users org prompt+optimized prompt
+def get_user_input_and_optimized_prompts(user_id):
+   try:
+      # query the database
+      response = supabase.table("prompt").select("*").eq("user_id", user_id).execute()
+
+      # if there is data, return it
+      # need to figure what to do if there is no data... hmmhmmhmm
+      return response.data[0]
+      
+   # double check this later to see if this exception is the correct one!
+   except Exception as e:
+      raise RuntimeError(f"Database error: {str(e)}")
+   
+# return a specific users 1 prompt + optimized version of it
+def get_user_prompt_w_optimized(user_id, prompt_id):
+   try:
+      # query the database
+      response = supabase.table("prompt").select("*, optimized_prompts(*)").eq("user_id", user_id).execute()
+
+      # if there is data, return it
+      # need to figure what to do if there is no data... hmmhmmhmm
+      return response.data[0]
+      
+   # double check this later to see if this exception is the correct one!
+   except Exception as e:
+      raise RuntimeError(f"Database error: {str(e)}")
+
+
+### POST METHODS
+
+# send a prompt to the database :3
+
+# send an optimized prompt to the database :3 (but i also think that this should just be a regular function, but who knows what can happen?)
+## trying to think of a way this can be used... :/
+
+### DELETE METHODS
+# delete user 
+## since enzo is security guy, lets discuss how we should do this bc in a real app they have like secuirty measures to prevent accidental deletion
+## but this might be a later issue!
+
+# delete prompt (which also should delete the optimized prompt who is with me!)
+
+# delete optimize (don't even think we need this but it's alwaus good... to have soemthing!)
+
+### UPDATE METHOD
+# update user: self explanatory...
+
+# update prompt/optimized prompt: not sure as a now? i thikn if the user modifies the original prompt. but i think that should just not be able to go thru...
+
